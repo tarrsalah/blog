dab78baa9b51eee4568f918da23cb3aa38469d8b
tarrsalah <tarrsalah@gmail.com>
2019-04-05 17:49:36 +0100

    Updates

---
+++ component---src-pages-index-js-8cc5f5f1291eda40e459.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{141:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-8cc5f5f1291eda40e459.js.map
---
+++ pages-manifest-6c2010797256217520b1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{142:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-hello-js",jsonName:"hello-399",path:"/hello/"}],dataPaths:{"dev-404-page-5f9":"386/path---dev-404-page-5-f-9-fab-0SUcWyAf8ecbYDsMhQkEfPzV8","hello-399":"921/path---hello-399-f25-0SUcWyAf8ecbYDsMhQkEfPzV8",index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8"}}}}]);
+//# sourceMappingURL=pages-manifest-6c2010797256217520b1.js.map
